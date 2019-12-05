0be718ed10ce55442a596c492878fc82811a69c5
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-05 13:55:17 -0500

    Update dbconnection.php
    Update path to database based on where dbconnection is located

--- app/src/dbconnection.php
+++ app/src/dbconnection.php
@@ -1,6 +1,6 @@
 <?php
     try {
-					$db = new PDO("sqlite:".__DIR__."/blog.db");
+					$db = new PDO("sqlite:".__DIR__."/../../blog.db");
 					$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
 		} catch (Exception $e) {
 				echo $e->getMessage();
