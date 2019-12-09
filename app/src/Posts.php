efded7014f627df6ffb069f71945987085dbab83
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-09 15:13:29 -0500

    Update Posts.php
    Comment-out function for adding a post to posts table

--- app/src/Posts.php
+++ app/src/Posts.php
@@ -17,17 +17,17 @@ class Posts {
 		return $results->fetchAll(PDO::FETCH_ASSOC);
 	}
 	// Add a post to database
-	public function addPost() {
-		include __DIR__ . "/../src/dbconnection.php";
-		$sql = "INSERT INTO posts (title, date, body) VALUES(?, ?, ?, ?);
+	// public function addPost() {
+	// 	include __DIR__ . "/../src/dbconnection.php";
+	// 	$sql = "INSERT INTO posts (title, date, body) VALUES(?, ?, ?, ?);
 
-		try {
-			$results = $db->query($sql); 
-		} catch (Exception $e) {
-			echo $e->getMessage();
-			return array();
-		}
-		return $results->fetchAll(PDO::FETCH_ASSOC);
-	}
+	// 	try {
+	// 		$results = $db->query($sql); 
+	// 	} catch (Exception $e) {
+	// 		echo $e->getMessage();
+	// 		return array();
+	// 	}
+	// 	return $results->fetchAll(PDO::FETCH_ASSOC);
+	// }
 
 }
