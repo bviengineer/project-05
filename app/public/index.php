5c83b4f3e63cc9e25f2ad0e1dd127e0c0ec97a40
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-09 11:07:44 -0500

    index.php, route.php
    move test routes to routes.php

--- app/public/index.php
+++ app/public/index.php
@@ -19,16 +19,8 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
-// my test route 
-$app->get('/test/{name}', function(Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello World, $name");
-    return $response;
-});
-
 // my test db query
-$app->get('/posts', function(Request $request, Response $response, $args) {
-    // $name = $args['name'];
+$app->get('/posts', function(Request $request, Response $response) {
     include __DIR__ . "/../src/dbconnection.php";
     $sql = "SELECT * FROM posts ORDER BY id";
 
@@ -40,8 +32,8 @@ $app->get('/posts', function(Request $request, Response $response, $args) {
 	}
 	return $results->fetchAll(PDO::FETCH_ASSOC);
 
-    $response->write($results);
-    return $response;
+    return $response->write($results);
+    //return $response;
 });
 
 // Set up dependencies
--- app/src/routes.php
+++ app/src/routes.php
@@ -7,4 +7,20 @@ $app->get('/[{name}]', function ($request, $response, $args) {
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+}); 
+
+// my test route 
+$app->get('/hello/[{name}]', function(Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    //$name = $request->getQueryParams(['name']);
+    $response->getBody()->write("Hello World, $name");
+    //echo "<pre>";
+    return $response;
+    //echo "</pre>";
+});
+
+// my test route 
+$app->get('/test/{method}', function(Request $request, Response $response) {
+    $method = $request->getMethod();
+    return $method;
 });
