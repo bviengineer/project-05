ddedbd5fc37d780a736e7624d209c9678d2e127d
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-09 16:13:13 -0500

    Update Posts.php
    Add function to retrieve all posts with associated comment(s)

--- app/src/Posts.php
+++ app/src/Posts.php
@@ -16,6 +16,20 @@ class Posts {
 		}
 		return $results->fetchAll(PDO::FETCH_ASSOC);
 	}
+	// Retrieve all posts with associated comments
+	public function getFullPosts() {
+		include __DIR__ . "/../src/dbconnection.php";
+		$sql = 'SELECT posts.title, posts.date, posts.body, comments.name, comments.body 
+          	FROM posts
+        		LEFT OUTER JOIN comments on comments.id = posts.id';
+		try {
+			$results = $db->query($sql); 
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return array();
+		}
+		return $results->fetchAll(PDO::FETCH_ASSOC);
+	}
 	// Add a post to database
 	// public function addPost() {
 	// 	include __DIR__ . "/../src/dbconnection.php";
