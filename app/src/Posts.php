11745d21e59e1252bb35cb0295321e65ff4699cb
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-09 12:18:20 -0500

    Update Posts.php
    Add function that retrieves all posts from database

--- app/src/Posts.php
+++ app/src/Posts.php
@@ -1,5 +1,19 @@
 <?php
 
 class Posts {
+// my test db query
+
+	public function getPosts() {
+		include __DIR__ . "/../src/dbconnection.php";
+		$sql = "SELECT * FROM posts ORDER BY id";
+
+		try {
+			$results = $db->query($sql); 
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return array();
+		}
+		return $results->fetchAll(PDO::FETCH_ASSOC);
+	}
 
 }
