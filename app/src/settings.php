b37b0a850eacc9b0ff2015d3e605bc398f3d38c8
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-10 17:00:53 -0500

    Update settings.php
    Remove databas setting

--- app/src/settings.php
+++ app/src/settings.php
@@ -12,12 +12,6 @@ return [
         'logger' => [
             'name' => 'slim-app',
             'path' => __DIR__ . '/../logs/app.log',
-				],
-				// Database settings
-        // 'db' => [
-				// 	'driver' => 'sqlite',
-				// 	'database' => __DIR__.'/../../blog.db',
-				// 	'prefix' => ''
-				// ], 
+				], 
     ], // closing square bracket for 'settings'
 ]; // closing squre bracket for bracket after return keyword
