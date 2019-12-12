a37621dd80be5bc97d93e9b79bcca05e184fd0ac
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-12 12:30:49 -0500

    Update Comments.php
    Setup PDO database connection var and constructor
    -Modyify sql query to use PDO connection

--- app/src/Comments.php
+++ app/src/Comments.php
@@ -1,19 +1,21 @@
 <?php
 
 class Comments {
-// my test db query
+
+	// Database variable 
+protected $db; 
+
+// Databse PDO Construtor Method
+public function __construct(\PDO $db) {
+	$this->db = $db; 
+}
 
 	// Retrieve all comments
 	public function getComments() {
-		include __DIR__ . "/../src/dbconnection.php";
 		$sql = "SELECT * FROM comments ORDER BY id";
 
-		try {
-			$results = $db->query($sql); 
-		} catch (Exception $e) {
-			echo $e->getMessage();
-			return array();
-		}
+		$results = $this->db->prepare($sql); 
+		$results->execute();
 		return $results->fetchAll(PDO::FETCH_ASSOC);
 	}
 }
