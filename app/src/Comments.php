fdd8a6beb42cc80caec445b8a4b8e17953f3cbdd
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-18 12:35:35 -0500

    Update Comments.php
    -Add parameter to getComments method
    -Add bindParam code

--- app/src/Comments.php
+++ app/src/Comments.php
@@ -8,16 +8,19 @@ class Comments {
 	// Database variable 
 protected $db; 
 
-// Databse PDO Construtor Method
-public function __construct(\PDO $db) {
-	$this->db = $db; 
-}
-
+	// Databse PDO Construtor Method
+	public function __construct(\PDO $db) {
+		$this->db = $db; 
+	}
 	// Retrieve all comments
-	public function getComments() {
-		$sql = "SELECT * FROM comments ORDER BY id";
+	public function getComments($postId) {
+		$sql = "SELECT * 
+				FROM comments 
+				WHERE post_id = :id";
+				// ORDER BY id";
 
 		$results = $this->db->prepare($sql); 
+		$results->bindParam('post_id', $postId);
 		$results->execute();
 		return $results->fetchAll(PDO::FETCH_ASSOC);
 	}
