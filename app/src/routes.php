ef915e94388288353720e19e5bf240567f55bdf9
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-18 14:07:09 -0500

    Update routes.php
    Outputing code for troubleshooting

--- app/src/routes.php
+++ app/src/routes.php
@@ -30,17 +30,20 @@ $app->get('/post/{id}', function($request, $response, $args) {
   $comm = new Comments($this->db);
   $postComm = $comm->getComments($args['id']);
 
-  echo "<pre>";
-  var_dump($postComm);
-  echo "</pre>";
-
   // Assign a key to the args array & store results of query
   $args['post'] = $results;
   $args['comments'] = $postComm;
+
+  echo "<pre>";
+  var_dump($args['comments']);
+  echo "</pre>";
   //var_dump($args['id']);
   // $args['post'] = $args['post'][0];
   // echo "<pre>";
   // var_dump($args['post']);
+  // echo "</pre>";
+  // echo "<pre>";
+  // var_dump($args);
   // echo "</pre>";
   
   // Render results
