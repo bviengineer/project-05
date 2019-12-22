c589ad3a7e1edbfd7486e3586c6d941943b34c6c
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-22 10:49:20 -0500

    Update PostsTags.php
    Add method to add tags to database based on post_id & tag_id

--- app/src/PostsTags.php
+++ app/src/PostsTags.php
@@ -17,5 +17,15 @@ class PostsTags {
 		$results = $this->db->prepare("SELECT * FROM posts_tags ORDER BY post_id"); 
 		$results->execute();
 		return $results->fetchAll(PDO::FETCH_ASSOC);
-	}
+    }
+    public function addTags($post_id, $tags_id) {
+        $sql = "INSERT INTO posts_tags (title, date, body) VALUES(:title, :date, :body)";
+
+		$results = $this->db->prepare($sql); 
+		$results->bindParam(':title', $title, PDO::PARAM_STR);
+		$results->bindParam(':date', $date, PDO::PARAM_STR);
+		$results->bindParam(':body', $body, PDO::PARAM_STR);
+		$results->execute();
+		return true;
+    }
 }
