17a6e4a5893af1bfdacc4e196b560bb902e15d89
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-12-22 23:10:15 -0500

    Update Tags.php
    Update query to select all data from tags table

--- app/src/Tags.php
+++ app/src/Tags.php
@@ -14,7 +14,7 @@ class Tags {
 	}
 	// Get all tags
 	public function getTags($tag_id) {
-		$results = $this->db->prepare("SELECT name FROM tags WHERE id = :tag_id"); 
+		$results = $this->db->prepare("SELECT * FROM tags WHERE id = :tag_id"); 
 		$results->bindParam(':tag_id', $tag_id, PDO::PARAM_INT);
 		$results->execute();
 		return $results->fetchAll(PDO::FETCH_ASSOC);
